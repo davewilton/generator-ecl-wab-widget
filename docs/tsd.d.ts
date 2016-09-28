919f19222ce5e82e4da72a3173568ca4eabd9743
David Wilton <dave_wilton@msn.com>
2016-09-28 01:02:38 +0100

    added sampe tsd.d.ts

---
+++ docs/tsd.d.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../node_modules/dojo-typings/dojo/1.11/index.d.ts" />
+/// <reference path="../node_modules/dojo-typings/dojo/1.11/modules.d.ts" />
+/// <reference path="../node_modules/dojo-typings/dijit/1.11/index.d.ts" />
+/// <reference path="../node_modules/dojo-typings/dijit/1.11/modules.d.ts" />
+/// <reference path="../node_modules/dojo-typings/dojox/1.11/index.d.ts" />
+/// <reference path="../node_modules/dojo-typings/dojox/1.11/modules.d.ts" />
+
+/// <reference path="scripts/typings/arcgis-js-api/arcgis-js-api.d.ts" />
+/// <reference path="scripts/typings/jasmine/jasmine.d.ts" />
+
+/// <reference path="jimu.js/jimu.d.ts" />
+
